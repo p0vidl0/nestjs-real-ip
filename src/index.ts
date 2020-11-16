a9d31827ae82e3145f7ba885b45cd9e708c667fa
Alex Kalashnikov <nixomsk@gmail.com>
2020-11-16 06:24:38 +0600

    Add RealIp synonym for RealIP

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,5 @@ export const RealIP = createParamDecorator((data: unknown, ctx: ExecutionContext
   const request = ctx.switchToHttp().getRequest();
   return requestIp.getClientIp(request);
 });
+
+export const RealIp = RealIP;
